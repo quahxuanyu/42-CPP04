424a15d8dfb7cbfed213bc3c617ca7426173f182
Xuanyu Quah <xquah@u82z03s04.42kl.edu.my>
2025-06-12 15:57:45 +0800

    added ex03

---
+++ ex03/AMateria.hpp
@@ -0,0 +1,23 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+#include <iostream>
+#include "ICharacter.hpp"
+
+class AMateria {
+    protected:
+       std::string type; 
+    public:
+        AMateria(std::string const &type);
+
+        AMateria();
+        AMateria(const AMateria &other);
+        AMateria &operator=(const AMateria &other);
+        ~AMateria();
+
+        std::string const &getType() const; // Returns the Materia type
+
+        virtual AMateria *clone() const = 0;
+        virtual void use(ICharacter &target);
+};
+
+#endif
