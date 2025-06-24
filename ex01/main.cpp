e4256c5fe97d6c933e14ab0f2ccd0642582b69aa
Xuanyu Quah <xquah@u82z05s02.42kl.edu.my>
2025-06-24 16:14:19 +0800

    modified main.cpp and changed WrongAnimal

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,9 +6,32 @@
 
 int main()
 {
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	delete j;//should not create a leak
-	delete i;
-	return 0;
+	std::cout << "-----------ANIMAL TEST------------" << std::endl;
+	int size = 10;
+	const Animal *animal_arr[size];
+
+	for (int i = 0; i < size / 2; i++)
+		animal_arr[i] = new Cat();
+	for (int i = size / 2; i < size; i++)
+		animal_arr[i] = new Dog();
+	for (int i = 0; i < size; i++)
+		animal_arr[i]->makeSound();
+	for (int i = 0; i < size; i++)
+		delete animal_arr[i];
+
+
+	std::cout << "\n-----------DEEP COPT TEST------------" << std::endl;
+	{
+		Cat a;
+		Cat b = a;  // Copy constructor
+		Cat c;
+		c = a;      // Assignment operator
+	}
+	{
+		Dog a;
+		Dog b = a;  // Copy constructor
+		Dog c;
+		c = a;      // Assignment operator
+	}
+
 }
